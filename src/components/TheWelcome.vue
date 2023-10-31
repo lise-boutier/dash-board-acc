2c4dd0dbde600a486874b9fa91a9035c3e97e918
Lise <lboutier@seeyousun.fr>
2023-10-31 10:05:04 +0100

    correction

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script setup>
-    import { onMounted, onUpdated } from 'vue';
+    import { onMounted } from 'vue';
     import * as d3 from 'd3'
 
     // import data1 from '../daily_prod_data.json'
@@ -30,25 +30,25 @@
         }
     }
     
-    // Declare the x (horizontal position) scale.
-    let x = d3.scaleBand()
-        .domain(newArray.map(data => data.timestamp))
-        .range([marginLeft, width - marginRight])
-        .padding(0.1);
-
-    // Declare the y (vertical position) scale.
-    let y = d3.scaleLinear()
-        .domain([0, d3.max(newArray, (d) => d.prod_kwh)])
-        .range([height - marginBottom, marginTop]);
-
-    // Create the SVG container.
-    let svg = d3.select("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("viewBox", [0, 0, width, height])
-        .attr("style", "max-width: 100%; height: auto;");
-        
     function initGraph(){
+
+        // Declare the x (horizontal position) scale.
+        let x = d3.scaleBand()
+            .domain(newArray.map(data => data.timestamp))
+            .range([marginLeft, width - marginRight])
+            .padding(0.1);
+    
+        // Declare the y (vertical position) scale.
+        let y = d3.scaleLinear()
+            .domain([0, d3.max(newArray, (d) => d.prod_kwh)])
+            .range([height - marginBottom, marginTop]);
+    
+        // Create the SVG container.
+        let svg = d3.select("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("viewBox", [0, 0, width, height])
+            .attr("style", "max-width: 100%; height: auto;");
 
         // Add a rect for each bar.
         svg.append("g")
@@ -81,40 +81,6 @@
         // Return the SVG element.
         return svg.node();
     }
-
-    function update(data) {
-
-        // Update the X axis
-        x.domain(newArray.map(function(d) { return d.timestamp; }))
-        .call(d3.axisBottom(x))
-
-        // Update the Y axis
-        y.domain([0, d3.max(oneArray, function(d) { return d.prod_kwh }) ])
-        .transition().duration(1000).call(d3.axisLeft(y));
-
-        // Create the u variable
-        var u = svg.selectAll("rect")
-        .data(data)
-
-        u.enter()
-        .append("rect") // Add a new rect for each new elements
-        .merge(u) // get the already existing elements as well
-        .transition() // and apply changes to all of them
-        .duration(1000)
-            .attr("x", function(d) { return x(d.timestamp); })
-            .attr("y", function(d) { return y(d.prod_kwh); })
-            .attr("width", x.bandwidth())
-            .attr("height", function(d) { return height - y(d.prod_kwh); })
-            .attr("fill", "#69b3a2")
-
-        // If less group in the new dataset, I delete the ones not in use anymore
-        u.exit()
-        .remove()
-    }
-
-    onUpdated(() => {
-        update()
-    }),
 
     onMounted(() => {
 
